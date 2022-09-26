bfbcb78bafd67d9474a146c36abc0fc7f5ec0836
an1keenko <ihar.anikeyenka@gmail.com>
2022-09-27 00:35:38 +0200

    created src 2.0

--- tests/module.test.js
+++ tests/module.test.js
@@ -1,19 +1,12 @@
 import { randProductName, randNumber } from '@ngneat/falso';
-
-function addProduct(name, price) {
-  const productNameRegexNoSpace = /^\S*$/g; // no white-space allowed
-
-  if (!productNameRegexNoSpace.test(name)) {
-    return false;
-  }
-
-  return typeof price === 'number';
-}
+import { addProduct } from '../src/scripts/module';
 
 test('When adding new valid product, get successful confirmation', () => {
   const name = randProductName();
   const price = randNumber();
+
   // Generated random input: { 'Handmade Granite Cheese', 704809 }
+  console.log(name, price);
   expect(addProduct(name, price)).toBe(true);
   // Test failed, the random input triggered some path we never planned for.
   // We discovered a bug early!
