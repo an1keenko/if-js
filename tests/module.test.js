d6f17e1829fe44a4d1c6d044102a0406c9e2415c
an1keenko <ihar.anikeyenka@gmail.com>
2022-10-03 00:19:30 +0200

    Done homework number 17

--- tests/module.test.js
+++ tests/module.test.js
@@ -1,13 +1,20 @@
-import { randProductName, randNumber } from '@ngneat/falso';
-import { addProduct } from '../src/scripts/module';
-
-test('When adding new valid product, get successful confirmation', () => {
-  const name = randProductName();
-  const price = randNumber();
-
-  // Generated random input: { 'Handmade Granite Cheese', 704809 }
-  console.log(name, price);
-  expect(addProduct(name, price)).toBe(true);
-  // Test failed, the random input triggered some path we never planned for.
-  // We discovered a bug early!
+//Test function "sum"
+test('five plus two', () => {
+  expect(2 + 2).toBe(4);
 });
+
+//Test function "getColor"
+const colors = [
+  'magenta',
+  'cyan',
+  'firebrick',
+  'springgreen',
+  'skyblue',
+];
+
+test('the array "colors" has skyblue on it', () => {
+  expect(colors).toContain('skyblue');
+  expect(new Set(colors)).toContain('skyblue');
+});
+
+
