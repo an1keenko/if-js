e2552ee9036fdeddcbc90f907cb6d506332a8196
an1keenko <ihar.anikeyenka@gmail.com>
2022-10-09 02:09:26 +0200

    Done homework 18

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,62 +1,74 @@
-//1
+//1(Change date format)
+function getDate(date) {
+  const dateArr = date.split('-');
 
-export function sum(x) {
-  return function (y) {
-    return x + y;
-  };
+  [dateArr[0], dateArr[2]] = [dateArr[2], dateArr[0]];
+
+  return dateArr.join('.');
 }
 
-console.log(sum(5)(2));
+console.log(getDate('2020-11-26'));
 
 //2
-//getColor
-const text1 = document.getElementById('text1');
-const text2 = document.getElementById('text2');
-const text3 = document.getElementById('text3');
 
-const colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
+const data = [
+  {
+    country: 'Russia',
+    city: 'Saint Petersburg',
+    hotel: 'Hotel Leopold',
+  },
+  {
+    country: 'Spain',
+    city: 'Santa Cruz de Tenerife',
+    hotel: 'Apartment Sunshine',
+  },
+  {
+    country: 'Slowakia',
+    city: 'Vysokie Tatry',
+    hotel: 'Villa Kunerad',
+  },
+  {
+    country: 'Germany',
+    city: 'Berlin',
+    hotel: 'Hostel Friendship',
+  },
+  {
+    country: 'Indonesia',
+    city: 'Bali',
+    hotel: 'Ubud Bali Resort&SPA',
+  },
+  {
+    country: 'Netherlands',
+    city: 'Rotterdam',
+    hotel: 'King Kong Hostel',
+  },
+  {
+    country: 'Marocco',
+    city: 'Ourika',
+    hotel: 'Rokoko Hotel',
+  },
+  {
+    country: 'Germany',
+    city: 'Berlin',
+    hotel: 'Hotel Rehberge Berlin Mitte',
+  },
+];
 
-text1.addEventListener(
-  'click',
-  (function () {
-    for (let i = 0; i <= colors.length; ) {
-      return function (event) {
-        event.target.style.color = colors[i];
-        i++;
-        if (i >= colors.length) {
-          i = 0;
-        }
-      };
-    }
-  })()
-);
+function search(name) {
+  const objects = data.filter(
+    (item) =>
+      item.country.includes(name) ||
+      item.city.includes(name) ||
+      item.hotel.includes(name)
+  );
 
-text2.addEventListener(
-  'click',
-  (function () {
-    for (let i = 0; i <= colors.length; ) {
-      return function (event) {
-        event.target.style.color = colors[i];
-        i++;
-        if (i >= colors.length) {
-          i = 0;
-        }
-      };
-    }
-  })()
-);
+  const result = [];
 
-text3.addEventListener(
-  'click',
-  (function () {
-    for (let i = 0; i <= colors.length; ) {
-      return function (event) {
-        event.target.style.color = colors[i];
-        i++;
-        if (i >= colors.length) {
-          i = 0;
-        }
-      };
-    }
-  })()
-);
+  for (let i = 0; i < objects.length; i++) {
+    result.push(...Object.values(objects[i]));
+  }
+
+  return result;
+}
+
+console.log(search('Germany'));
