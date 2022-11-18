43809dcd03d827b7ba6437aac729bc371291b4af
an1keenko <ihar.anikeyenka@gmail.com>
2022-11-18 18:55:57 +0100

    Done homework 22

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,91 +1,44 @@
-//1
+const text1 = document.getElementById('text1');
+const text2 = document.getElementById('text2');
+const text3 = document.getElementById('text3');
 
-class User {
-  constructor(firstName, lastName) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-  }
-
-  get fullName() {
-    return this.firstName + ' ' + this.lastName;
-  }
-}
-
-class Student extends User {
-  constructor(firstName, lastName, admissionYear, courseName) {
-    super(firstName, lastName);
-    this.admissionYear = admissionYear;
-    this.courseName = courseName;
-  }
-
-  get course() {
-    const currentYear = new Date().getFullYear();
-    return currentYear - this.admissionYear;
-  }
-}
-
-const studentsData = [
-  {
-    firstName: 'Василий',
-    lastName: 'Петров',
-    admissionYear: 2019,
-    courseName: 'Java',
+const colors = {
+  data: ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'],
+  current: 0,
+  [Symbol.iterator]() {
+    return this;
   },
-  {
-    firstName: 'Иван',
-    lastName: 'Иванов',
-    admissionYear: 2018,
-    courseName: 'JavaScript',
-  },
-  {
-    firstName: 'Александр',
-    lastName: 'Федоров',
-    admissionYear: 2017,
-    courseName: 'Python',
-  },
-  {
-    firstName: 'Николай',
-    lastName: 'Петров',
-    admissionYear: 2019,
-    courseName: 'Android',
-  },
-];
-
-class Students {
-  constructor(students) {
-    this.students = [];
-
-    for (let i = 0; i < students.length; i++) {
-      this.students.push(
-        new Student(
-          studentsData[i].firstName,
-          studentsData[i].lastName,
-          studentsData[i].admissionYear,
-          studentsData[i].courseName,
-        ),
-      );
+  next() {
+    if (this.current < this.data.length) {
+      return {
+        done: false,
+        value: this.data[this.current++],
+      };
     }
-  }
-
-  get getInfo() {
-    const result = [];
-
-    this.students.sort((a, b) => (a.admissionYear < b.admissionYear ? 1 : -1));
-
-    for (let i = 0; i < this.students.length; i++) {
-      result.push(
-        this.students[i].fullName +
-          ' - ' +
-          this.students[i].courseName +
-          ', ' +
-          this.students[i].course,
-      );
+    if (this.current <= this.data.length) {
+      this.current = 0;
+      return {
+        done: false,
+        value: this.data[this.current],
+      };
     }
+  },
+};
 
-    return result;
-  }
+function giveColor(colors) {
+  return function(event) {
+    event.target.style.color = colors.next().value;
+    return event;
+  };
 }
 
-const students = new Students(studentsData);
+text1.addEventListener('click', giveColor({...colors}));
+text2.addEventListener('click', giveColor({...colors}));
+text3.addEventListener('click', giveColor({...colors}));
 
-console.log(students.getInfo);
+
+
+
+
+
+
