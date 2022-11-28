e5208e4d44837a974f976337a8a781bda6861e36
an1keenko <ihar.anikeyenka@gmail.com>
2022-11-28 12:33:47 +0100

    Fixed bugs. Lesson 10

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,34 +1,4 @@
-const data = [
-  {
-    id: '71ce9eac-e9b9-44f0-a342-9ff0b565f3b7',
-    name: 'Hotel Leopold',
-    city: 'Saint Petersburg',
-    country: 'Russia',
-    imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/hotel-leopold_mflelk.jpg',
-  },
-  {
-    id: 'aa560608-a879-48a7-80b6-deff2806b250',
-    name: 'Apartment Sunshine',
-    city: 'Santa  Cruz de Tenerife',
-    country: 'Spain',
-    imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379364/fe/apartment-sunshine_vhdlel.jpg',
-  },
-  {
-    id: '1d88fefe-edf1-4cda-844a-babbf29bb2b3',
-    name: 'Villa Kunerad',
-    city: 'Vysokie Tatry',
-    country: 'Slowakia',
-    imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/villa-kunerad_gdbqgv.jpg',
-  },
-  {
-    id: 'a2bf824d-edd8-41f0-8b70-244334086ab4',
-    name: 'Hostel Friendship',
-    city: 'Berlin',
-    country: 'Germany',
-    imageUrl: 'https://res.cloudinary.com/intellectfox/image/upload/v1610379364/fe/hostel-friendship_aw6tn7.jpg',
-  },
-];
-
+import { data } from "./data.js";
 
 const items = document.getElementsByClassName('homes__items')[0];
 
@@ -39,3 +9,14 @@ data.forEach((i) => {
   <h4 class="homes__subtitle"> ${i.city}, <br /> ${i.country}</h4>
 </div>`;
 });
+
+for(const elem of document.querySelectorAll('*')) {
+  elem.addEventListener('click', event => {
+    console.log(`Capturing: ${event.currentTarget}`);
+  }, true);
+  elem.addEventListener('click', event => {
+    console.log(`Bubbling: ${event.currentTarget}`);
+  });
+}
+
+
